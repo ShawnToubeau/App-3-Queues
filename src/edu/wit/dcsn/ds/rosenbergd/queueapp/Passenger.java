c8ac2ddbfbec18e87d20b18622a5166b5372f071
Shawn Toubeau <shawntoubeau@gmail.com>
2017-10-28 09:56:29 -0400

    Generates stations with random number of passenger.

--- src/edu/wit/dcsn/ds/rosenbergd/queueapp/Passenger.java
+++ src/edu/wit/dcsn/ds/rosenbergd/queueapp/Passenger.java
@@ -1,34 +1,46 @@
+package edu.wit.dcsn.ds.rosenbergd.queueapp;
+
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Objects;
 import java.util.Random;
 
 public class Passenger {
 
+    //counter used to assign unique ID
     static int counter;
 
     int passengerID;
     int source;
     int destination;
 
+    //passenger constructor
+    //arguments: starting station, list of station locations
     Passenger(int startingStation, ArrayList<Integer> stationList) {
 
-        ArrayList<Integer> stationListCopy = new ArrayList<>();
+        ArrayList<Integer> stationListCopy = new ArrayList<>(stationList);
 
+        //assigns unique ID to passenger
         this.passengerID = counter++;
+
+        //sets the passengers starting station
         this.source = startingStation;
+
+        //removes the passenger's starting location from list of station locations
         stationListCopy.remove(Integer.valueOf(this.source));
+        //sets passenger destination as random station location
         this.destination = stationListCopy.get(new Random().nextInt(stationListCopy.size()));
     }
 
+    //gets passenger's ID
     public int getPassengerID() {
         return this.passengerID;
     }
 
+    //gets passenger's starting station
     public int getSource() {
         return this.source;
     }
 
+    //gets passenger's destination
     public int getDestination() {
         return this.destination;
     }
--- src/edu/wit/dcsn/ds/rosenbergd/queueapp/TrainRoute.java
+++ src/edu/wit/dcsn/ds/rosenbergd/queueapp/TrainRoute.java
@@ -1,5 +1,6 @@
+package edu.wit.dcsn.ds.rosenbergd.queueapp;
+
 import java.util.ArrayList;
-import java.util.Collection;
 
 /**
  * Sets the collections for trains and stations and determines the train's route
@@ -8,14 +9,15 @@ import java.util.Collection;
  */
 public class TrainRoute {
 
+    int length;
+    String style;
 
+    public TrainRoute(int length, String style) {
+        this.length = length;
+        this.style = style;
+    }
     ArrayList<Object> route1 = new ArrayList<>();
     ArrayList<Object> stationCollection = new ArrayList<>();
     //collection for trains
     //collection for stations
-
-    public void whichDirection(){
-
-
-    }
 }
